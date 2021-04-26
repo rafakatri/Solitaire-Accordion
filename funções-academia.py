6dcc5a16d85ce8758a62f8495ce7b44736e67412
rafakatri <rkatri20032@gmail.com>
2021-04-26 14:15:15 -0300

    Função 3: extrai valor

--- funções-academia.py
+++ funções-academia.py
@@ -22,3 +22,12 @@ def extrai_naipe(carta):
     for i in naipe:
         if i in carta:
             return i  
+
+# Função 3 --> Extrair valor da carta
+
+def extrai_valor(carta):
+    valor = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    for el in valor:
+        if el in carta:
+            return el
+            break 
