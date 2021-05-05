b5d889b33cd9dc34a802455869443cb12085c72b
rafakatri <rkatri20032@gmail.com>
2021-05-05 14:50:14 -0300

    Esqueleto de uma partida

--- jogo.py
+++ jogo.py
@@ -1,1 +1,19 @@
-import funçõesAcademia
+from funçõesAcademia import *
+
+baralho = cria_baralho()
+
+while possui_movimentos_possiveis(baralho) == True:
+    i=1
+    for el in baralho:
+        print("{0}. {1}".format(i,el))
+        i += 1
+    perg2 = int(input("Escolha uma carta para empilhar (pelo número) "))
+    mov = lista_movimentos_possiveis(baralho, perg2-1)
+    if mov != []:
+        print('Escolhas possíveis:')
+        for el in mov:
+            print(str(el) + ". "+ str(baralho[el-1]))
+        perg3 = int(input('Escolha um lugar para ser empilhado (pelo número)'))
+        baralho = empilha(baralho, perg2-1, perg3-1)
+    else:
+        print('Não é possível fazer um movimento, escolha outra carta')
