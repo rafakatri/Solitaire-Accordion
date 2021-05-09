724ad162740ed4424d1eb29bef1fe91088783855
NatanLewi <natan.lewi@terra.com.br>
2021-05-09 19:52:25 -0300

    Testa o input da primeira carta selecionada + correções

--- jogo.py
+++ jogo.py
@@ -7,16 +7,19 @@ while True:
         for el in baralho:
             print("{0}. {1}".format(i,el))
             i += 1
-        perg2 = int(input("Escolha uma carta para empilhar (pelo número) "))
-        mov = lista_movimentos_possiveis(baralho, perg2-1)
-        if mov != []:
-            print('Escolhas possíveis:')
-            for el in mov:
-                print(str(el) + ". "+ str(baralho[el-1]))
-            perg3 = int(input('Escolha um lugar para ser empilhado (pelo número)'))
-            baralho = empilha(baralho, perg2-1, perg3-1)
-        else:
-            print('Não é possível fazer um movimento, escolha outra carta')
-    perg = input('Quer iniciar uma rodada? s/n')
+        try:
+            perg2 = int(input("Escolha uma carta para empilhar (pelo número) "))
+            mov = lista_movimentos_possiveis(baralho, perg2-1)
+            if mov != []:
+                print('Escolhas possíveis:')
+                for el in mov:
+                    print(str(el) + ". "+ str(baralho[el-1]))
+                perg3 = int(input('Escolha um lugar para ser empilhado (pelo número) '))
+                baralho = empilha(baralho, perg2-1, perg3-1)
+            else:
+              print('Não é possível fazer um movimento, escolha outra carta')
+        except:
+            print('Opção inválida. Digite um número inteiro.')
+    perg = input('Quer iniciar uma rodada? s/n ')
     if perg == 'n':
-        pass
+        break
